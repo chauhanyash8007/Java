df5936a74286b32f69acba681dcfbfaf0aef98c1
Yash Chauhan <chauhanyash8007@gamil.com>
2024-01-04 21:48:31 +0530

    new java file

---
+++ Java1.java
@@ -0,0 +1,10 @@
+// bolierplate code + output
+
+public class Java1{
+    public static void main(String args[]){
+        System.out.print("Hello World");               // for single line 
+        System.out.println("Hello World");             // for line Break
+        System.out.print("Hello World \n");             // for line Break
+    }
+}
+
