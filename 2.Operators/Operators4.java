7b0042e2b789a19c13cc119328352575ded2b059
Yash Chauhan <chauhanyash8007@gamil.com>
2024-01-06 22:00:02 +0530

    New Folder Added 2.Operators

---
+++ 2.Operators/Operators4.java
@@ -0,0 +1,18 @@
+// Logical Operator
+
+public class Operators4 {
+    public static void main(String[] args) {
+        System.out.println((3>2 && 5>0));
+        System.out.println((3>2 && 5<0));
+        System.out.println((3<2 && 5>0));
+        System.out.println((3<2 && 5<0));
+
+        System.out.println((3>2 || 5>0));
+        System.out.println((3>2 || 5<0));
+        System.out.println((3<2 || 5>0));
+        System.out.println((3<2 || 5<0));
+
+        System.out.println(!(3>2));
+        System.out.println(!(3<2));
+    }
+}
---
+++ 2.Operators/Operators5.java
@@ -0,0 +1,31 @@
+//Assignment Operators
+
+public class Operators5 {
+    public static void main(String[] args) {
+        int a = 10;
+        // a = a + 10;
+        a += 10;
+        System.out.println(a);
+
+        int b = 10;
+        // b = b - 10;
+        b -= 10;
+        System.out.println(b);
+    
+        int c = 10;
+        // c = c * 10;
+        c *= 10;
+        System.out.println(c);
+    
+        int d = 10;
+        // d = d / 10;
+        d /= 10;
+        System.out.println(d);
+    
+        int e = 10;
+        // e = e % 10;
+        e %= 10;
+        System.out.println(e);
+    }
+    
+}
