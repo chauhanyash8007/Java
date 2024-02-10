fba86b46e4bc270abefa119baf922b3a6cc7ee1a
Yash Chauhan <chauhanyash8007@gamil.com>
2024-02-10 20:31:04 +0530

    add in folder no 10

--- 10.STRINGS/Strings7.java
+++ 10.STRINGS/Strings7.java
@@ -4,7 +4,7 @@
 public class Strings7 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("");
-        for (char ch = 'a'; ch < 'z'; ch++) {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
             sb.append(ch);
         }
         System.out.println(sb);
